23e0c65a57548536c7a3b537667ff63cb8688ae1
kasra-l1r <kasra@di.ku.dk>
2024-03-20 18:04:30 +0100

    Renamed mysql example to only-primary

---
+++ dev/data-stores/mysql/only-primary/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "mysql" {
+  source      = "../../../../../modules/data-stores/mysql"
+  db_username = var.db_username
+  db_password = var.db_password
+  env_name    = "dev"
+  name        = "library"
+  table       = "books"
+}
+
