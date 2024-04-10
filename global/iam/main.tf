37a80e79b1c2c0f2ba055575fb78de48ab9509ae
Kasra <arnavaz.kasra@gmail.com>
2024-04-10 17:01:14 +0200

    formatting

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -17,9 +17,7 @@ provider "aws" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [
-    data.tls_certificate.github.certificates[0].sha1_fingerprint
-  ]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
 # Fetch GitHub's OIDC thumbprint
